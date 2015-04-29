e2a89ddeca3d8c6b410a9b3cb5e425913cb26bc9
Matt Leonawicz <matt.rstat@gmail.com>
2015-04-29 14:48:41 -0600

    Bring in outside app code for documentation

--- code/pm.R
+++ code/pm.R
@@ -6,7 +6,9 @@ proj.location <- matt.proj.path # Use default file location
 docDir <- c("Rmd/include", "md", "html", "Rnw", "pdf", "timeline")
 newProject(proj.name, proj.location, docs.dirs=docDir, overwrite=T) # create a new project
 
+appcode.files <- list.files("C:/github/shiny-apps/ar4ar5", pattern="\\.R$", full=TRUE, recursive=TRUE)
 rfile.path <- file.path(proj.location, proj.name, "code") # path to R scripts
+file.copy(appcode.files, paste0(rfile.path, "/appcode_", basename(appcode.files)), overwrite=TRUE)
 docs.path <- file.path(proj.location, proj.name, "docs")
 rmd.path <- file.path(docs.path, "Rmd")
 
