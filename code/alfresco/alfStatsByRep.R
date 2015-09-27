e68c29bdf422bd1d153eeef714b76ac9cba256cb
Matt Leonawicz <matt.rstat@gmail.com>
2015-09-27 17:38:26 -0600

    Refactoring to area by age, testing

--- code/alfresco/alfStatsByRep.R
+++ code/alfresco/alfStatsByRep.R
@@ -8,14 +8,15 @@ rasterOptions(chunksize=10^12,maxmemory=10^11)
 prep_alf_data <- function(dir, repid, years){
     rep.lab <- 
     p <- lapply(c("A", "V", "FireS"), function(p, r) gsub("expression","",paste(bquote(expression("^",.(p),".*.",.(paste0("_",c(0:199),"_")[r]),".*.tif$")),collapse="")), r=repid)
-    files <- lapply(1:length(plist), function(i, dir, p) list.files(dir, pattern=p[[i]], full=TRUE), dir=dir, p=p)
+    files <- lapply(1:length(p), function(i, dir, p) list.files(dir, pattern=p[[i]], full=TRUE), dir=dir, p=p)
     names(files) <- c("Age", "Veg", "FID")
     files.years <- as.numeric(gsub("FireScar_\\d+_", "", gsub(".tif", "", basename(files$FID))))
     ord <- order(files.years)
     files <- lapply(files, function(x, ord) x[ord], ord=ord)
     files <- lapply(files, function(x, file.yrs, keep.yrs) { k <- file.yrs %in% keep.yrs; if(any(k)) x[which(k)] else x }, file.yrs=files.years, keep.yrs=years)
     stopifnot(all.equal(length(files$Age), length(files$Veg), length(files$FID), length(years)))
-    c(files, years=as.integer(years))
+    files$years <- as.integer(years)
+    files
 }
 
 # @knitr getFireStats
@@ -26,7 +27,8 @@ getFireStats <- function(i, mainDir, years=NULL, cells, ...){
     d.fs <- vector("list", length(years))
     for(j in 1:length(x$years)){ # fire size by vegetation class
         v <- list(FID=getValues(raster(x$FID[j], band=2)), Veg=getValues(raster(x$Veg[j])))
-        d.fs[[j]] <- filter(cells, Cell %in% which(!is.na(v$FID))) %>% mutate(Vegetation=veg.labels[v$Veg[Cell]], FID=v$FID[Cell]) %>% group_by(LocGroup, Location, Vegetation, FID) %>% summarise(FS=length(Cell), Year=x$years[j])
+        d.fs[[j]] <- filter(cells, Cell %in% which(!is.na(v$FID))) %>% mutate(Vegetation=veg.labels[v$Veg[Cell]], FID=v$FID[Cell]) %>%
+            group_by(LocGroup, Location, Vegetation, FID) %>% summarise(FS=length(Cell), Year=x$years[j]) %>% setorder(LocGroup, Location, Vegetation, Year, FS)
         print(paste0("Replicate ", i, ": ", x$years[j]))
     }
     d.fs <- rbindlist(d.fs)[, Replicate:=as.integer(i)]
@@ -35,99 +37,61 @@ getFireStats <- function(i, mainDir, years=NULL, cells, ...){
 }
 
 # @knitr getAgeVegStats
-getAgeVegStats <- function(i, mainDir, denDir, years=NULL, cells.list, shp.names.list, n, n.samples, ...){
-    rep.lab <- paste0("_",c(0:199),"_")[i]
-    patA <- gsub("expression","",paste(bquote(expression("^A.*.",.(rep.lab),".*.tif$")),collapse=""))
-    filesA <- list.files(mainDir, pattern=patA, full=T)
-    patV <- gsub("expression","",paste(bquote(expression("^V.*.",.(rep.lab),".*.tif$")),collapse=""))
-    filesV <- list.files(mainDir, pattern=patV, full=T)
-    files.years <- as.numeric(substr(filesA,nchar(filesA)-7,nchar(filesA)-4))
-    if(is.numeric(years)) { p <- files.years %in% years; if(any(p)) { filesA <- filesA[which(p)]; filesV <- filesV[which(p)]; files.years <- files.years[which(p)] } }
-    years <- unique(files.years)
-    r <- getValues(raster(filesA[1])) # use as a template
-    data.ind <- which(!is.na(r))
-    x <- rapply(cells.list, f=function(x) lapply(1:length(filesA), function(x) 0L), classes="integer", how="replace")
-    
+getAgeVegStats <- function(i, mainDir, vegDir, years=NULL, cells, ...){
+    if(is.null(list(...)$veg.labels)) {
+        veg.labels <- c("Black Spruce", "White Spruce", "Deciduous", "Shrub Tundra", "Graminoid Tundra", "Wetland Tundra", "Barren lichen-moss", "Temperate Rainforest")
+    } else veg.labels <- list(...)$veg.labels
+    if(!is.numeric(list(...)$age.samples)) age.samples <- 1000 else age.samples <- list(...)$age.samples
+    x <- prep_alf_data(dir=mainDir, repid=i, years=years)
+    cells <- group_by(cells, LocGroup, Location)
+    r <- getValues(raster(x$Age[1])) # use as a template
+    idx <- which(!is.na(r))
+    idx.rmNA <- which(idx %in% 1:length(r))
     # Density estimation
-    denFun <- function(x, n, adj=0.25, min.zero=TRUE, diversify=FALSE){
+    denFun <- function(x, n=1000, adj=0.1, out="vector", min.zero=TRUE, diversify=FALSE){
         x <- x[!is.na(x)]
         lx <- length(x)
         if(diversify && length(unique(x))==1) x <- rnorm(max(10, lx), mean=x[1]) # diversify constant values
         if(lx==1) x <- x + c(-1:1) #single pixel of veg type, add and subtract one age year to make procedure possible
-        dif <- diff(range(x))
-        z <- density(x, adjust=adj, n=n, from=min(x)-max(1, 0.05*dif), to=max(x)+max(1, 0.05*dif))
-        if(min.zero && any(z$x < 0)) z <- density(x, adjust=adj, n=n, from=0, to=max(x)+max(1, 0.05*dif))
-        as.numeric(c(z$x, z$y))
+        b <- max(1, 0.05*diff(range(x)))
+        z <- density(x, adjust=adj, n=n, from=min(x)-b, to=max(x)+b)
+        if(min.zero && any(z$x < 0)) z <- density(x, adjust=adj, n=n, from=0, to=max(x)+b)
+        if(out=="vector") return(as.numeric(c(z$x, z$y))) else if(out=="list") return(z)
     }
     
-    # Bootstrapping
-    btfun <- function(p, n.samples=length(p)/2, n.boot=10000, interp=FALSE, n.interp=1000, ...){
-        if(!length(p)) return(p)
-        if(all(is.na(p))) return(rep(NA, n.boot))
-        p <- list(x=p[1:n.samples], y=p[(n.samples+1):(2*n.samples)])
-        if(interp && length(unique(p[1:n.samples])) > 1) p <- approx(p$x, p$y, n=n.interp)
-        p <- round(sample(p$x, n.boot, prob=p$y, rep=T), ...)
-        p
+    d.age <- vector("list", length(x$years))
+    for(j in 1:length(x$years)){
+        v <- list(Age=getValues(raster(x$Age[j]))[idx], Veg=getValues(raster(x$Veg[j]))[idx])
+        v$Age[v$Age < 0] <- v$Age[ v$Age < 0] + 2147483647 # temporary hack
+        d.age[[j]] <- filter(cells, Cell_rmNA %in% idx.rmNA) %>% mutate(Vegetation=veg.labels[v$Veg[Cell_rmNA]], Age=v$Age[Cell_rmNA]) %>%
+            group_by(LocGroup, Location, Vegetation, Age) %>% summarise(Area=length(Cell_rmNA), Year=x$years[j])
+        print(paste0("Replicate ", i, ": ", x$years[j]))
     }
-    
-    print("Beginning processing loop")
-    dlist <- vector("list", length(years))
-    for(j in 1:length(years)){
-        a <- getValues(raster(filesA[j]))[data.ind]
-        v <- getValues(raster(filesV[j]))[data.ind]
-        a[a<0] <- a[a<0] + 2147483647 # temporary hack
-        dlist1 <- vector("list", length(x))
-        for(l1 in 1:length(x)){
-            dlist2 <- vector("list", length(x[[l1]]))
-            for(l2 in 1:length(x[[l1]])){
-                cells.tmp <- cells.list[[l1]][[l2]]
-                if(!length(cells.tmp)) next
-                a.tmp <- a[cells.tmp]
-                v.tmp <- v[cells.tmp]
-                v.tmp.n <- tapply(cells.tmp, v.tmp, length)
-                den <- unlist(tapply(a.tmp, v.tmp, denFun, n=n.samples))
-                samp <- tapply(den, rep(1:(length(den)/(2*n.samples)), each=2*n.samples), btfun, n.samples=n.samples, n.boot=n.samples, interp=TRUE)
-                dlist2[[l2]] <- data.table(LocGroup=names(x)[l1], Location=names(x[[l1]])[l2], VegID=rep(as.integer(names(samp)), each=n.samples), VegArea=rep(v.tmp.n, each=n.samples), Val=unlist(samp), Year=as.integer(years[j]))
-            }
-            dlist1[[l1]] <- rbindlist(dlist2)
-        }
-        dlist[[j]] <- rbindlist(dlist1)
-        if(!is.data.table(dlist[[j]])){
-            print(paste("PROBLEM WITH REPLICATE", i, "YEAR", years[j]))
-            print(dlist[[j]])
-            print(paste0("Class of dlist[[", j, "]]:", class(dlist[[j]])))
-            stop(paste("PROBLEM WITH REPLICATE", i, "YEAR", years[j]))
-        }
-        print(paste("Replicate", i, "age and veg maps:", years[j]))
-    }
-    
-    x <- rbindlist(dlist)
-    
-    x$Replicate <- as.integer(i)
-    veg.area <- x[seq(1, nrow(x), by=n.samples)][, Val:=NULL]
-    setnames(veg.area, "VegArea", "Val")
-    veg.area[, Var := "Vegetated Area"]
-    setcolorder(veg.area, c("LocGroup", "Location", "VegID", "Var", "Val", "Year", "Replicate"))
-    locs <- unique(x$Location)
-    x[, VegArea := NULL]
-    x[, Replicate := NULL]
-    setkey(x, Location)
+    d.age <- rbindlist(d.age)[, Replicate:=as.integer(i)]
+    d.age <- group_by(d.age, Replicate, LocGroup, Location, Year, Vegetation) %>% setorder(Replicate, LocGroup, Location, Year, Vegetation, Age, Area)
+    #d.age <- summarise(d.age, Val=denFun(rep(Age, times=Area), n=age.samples, out="list")$x, Prob=denFun(rep(Age, times=Area), n=age.samples, out="list")$y)
+    #d.area <- summarise(d.age, Val=sum(Area))
+    #d.area[, Var:= "Vegetated Area"]
+    #setcolorder(d.area, c("LocGroup", "Location", "Vegetation", "Var", "Val", "Year", "Replicate"))
+    locs <- unique(d.age$Location)
+    setkey(d.age, Location)
     for(j in 1:length(locs)){
         obj.name.tmp <- paste0("age__", locs[j], "__rep", i)
-        assign(obj.name.tmp, x[locs[j]])
-        save(list=c("locs", obj.name.tmp), file=paste0(denDir, "/", obj.name.tmp, ".RData"))
+        assign(obj.name.tmp, d.age[locs[j]])
+        save(list=c("locs", obj.name.tmp), file=paste0(vegDir, "/", obj.name.tmp, ".RData"))
         print(paste(obj.name.tmp, "object", j, "of", length(locs), "saved."))
         rm(list=obj.name.tmp)
         gc()
     }
-    rm(x)
+    rm(d.age)
     gc()
-    print("Returning veg class areas data frame for subregions.")
-    if(!is.data.table(veg.area)){
-        print(paste("PROBLEM WITH REPLICATE", i))
-        print(veg.area)
-        print(paste("Class of veg.area:", class(veg.area)))
-        stop(paste("PROBLEM WITH REPLICATE", i))
-    }
-    veg.area
+    #print("Returning veg class areas data table for subregions.")
+    #if(!is.data.table(d.area)){
+    #    print(paste("PROBLEM WITH REPLICATE", i))
+    #    print(d.area)
+    #    print(paste("Class of d.area:", class(d.area)))
+    #    stop(paste("PROBLEM WITH REPLICATE", i))
+    #}
+    #d.area
+    return()
 }
--- code/alfresco/alfStatsByRep_Rmpi.R
+++ code/alfresco/alfStatsByRep_Rmpi.R
@@ -3,7 +3,8 @@
 args=(commandArgs(TRUE))
 if(!length(args)) q("no") else for(i in 1:length(args)) eval(parse(text=args[[i]]))
 
-if(!exists("model.index")) stop("Must provide model(s) as integer(s) 1:15, e.g., model.index=1:2")
+if(!exists("model.index")) stop("Must provide a model.index 1 to 15, e.g., model.index=1")
+stopifnot(length(model.index)==1)
 if(!exists("reps")) stop("Must provide replicates as integer(s) 1:200, e.g., reps=1:25")
 if(!exists("years")) years <- NULL # Assume all years if none specified
 if(!exists("Rmpi")) Rmpi <- TRUE
@@ -34,15 +35,6 @@ if(Rmpi){
 # Load data table of cell indices defining groups of shapefile polygons
 load("/workspace/UA/mfleonawicz/projects/DataExtraction/workspaces/shapes2cells_akcan1km2km.RData")
 cells <- filter(cells, Source=="akcan1km") %>% group_by %>% select(-Source) %>% group_by(LocGroup, Location)
-#load("/workspace/UA/mfleonawicz/projects/DataExtraction/workspaces/shapes2cells_AKCAN1km.RData")
-#load("/workspace/UA/mfleonawicz/projects/DataExtraction/workspaces/shapes2cells_AKCAN1km_rmNA.RData")
-#if(exists("locgroup")){
- #   print(paste("locgroup =", locgroup))
-	#cells_shp_list <- cells_shp_list[locgroup]
-	#cells_shp_list_rmNA <- cells_shp_list_rmNA[locgroup]
-	#region.names.out <- region.names.out[locgroup]
-	#n.shp <- sum(sapply(region.names.out, length))
-#}
 if(exists("locgroup")){
     cat("locgroup = "); cat(locgroup); cat("\n")
 	cells <- filter(cells, LocGroup %in% locgroup)
@@ -50,12 +42,12 @@ if(exists("locgroup")){
 
 dirs <- list.files("/big_scratch/apbennett/Calibration/FinalCalib", pattern=".*.sres.*.", full=T)
 mainDirs <- rep(paste0(dirs,"/Maps")[model.index], each=length(reps))
-modnames <- basename(dirname(mainDirs)) # Should only be one name at a time due to $ScenMod column naming below
-dir.create(ageDenDir <- file.path("/big_scratch/mfleonawicz/Rmpi/outputs/ageDensities", modnames[1]), recursive=T, showWarnings=F) # also assumes one model at a time
+modname <- basename(dirname(mainDirs))
+dir.create(vegDir <- file.path("/big_scratch/mfleonawicz/Rmpi/outputs/veg", modname), recursive=T, showWarnings=F)
 repid <- rep(reps,length(model.index))
 #veg.labels <- c("Black Spruce", "White Spruce", "Deciduous", "Shrub Tundra", "Graminoid Tundra", "Wetland Tundra", "Barren lichen-moss", "Temperate Rainforest")
 scen.levels <- c("SRES B1", "SRES A1B", "SRES A2", "RCP 4.5", "RCP 6.0", "RCP 8.5")
-mod.scen <- unlist(strsplit(modnames[1], "\\.")) # assume one model at a time
+mod.scen <- unlist(strsplit(modname, "\\."))
 
 # @knitr functions
 # Support functions
@@ -87,8 +79,8 @@ if(Rmpi){
 	mpi.bcast.Robj2slave(cells)
 	mpi.bcast.Robj2slave(years)
 	mpi.bcast.Robj2slave(mainDirs)
-	mpi.bcast.Robj2slave(modnames)
-	mpi.bcast.Robj2slave(ageDenDir)
+	mpi.bcast.Robj2slave(modname)
+	mpi.bcast.Robj2slave(vegDir)
 	mpi.bcast.Robj2slave(repid)
 	print("mpi.bcast.Robj2slave calls completed.")
 }
@@ -113,12 +105,12 @@ if(Rmpi){
 if(doFire){
     print("#### Compiling fire statistics... ####")
 	if(Rmpi){
-        fsv.dat <- mpi.remote.exec( getFireStats(i=repid[id], mainDir=mainDir, years=years, cells=selct(cells, -Cell_rmNA)) )
+        fsv.dat <- mpi.remote.exec( getFireStats(i=repid[id], mainDir=mainDir, years=years, cells=select(cells, -Cell_rmNA)) )
         fsv.dat <- rbindlist(fsv.dat)
     } else {
         len <- length(repid)
         if(len <= n.cores){
-            fsv.dat <- mclapply(repid, getFireStats, mainDir=mainDir, years=years, cells=selct(cells, -Cell_rmNA), mc.cores=n.cores)
+            fsv.dat <- mclapply(repid, getFireStats, mainDir=mainDir, years=years, cells=select(cells, -Cell_rmNA), mc.cores=n.cores)
             fsv.dat <- rbindlist(fsv.dat)
         } else {
             serial.iters <- ceiling(len/n.cores)
@@ -127,7 +119,7 @@ if(doFire){
             for(j in 1:serial.iters){
                 repid.tmp <- 1:n.cores2 + (j-1)*n.cores2
                 repid.tmp <- repid.tmp[repid.tmp <= max(repid)]
-                fsv.tmp <- mclapply(repid.tmp, getFireStats, mainDir=mainDir, years=years, cells=selct(cells, -Cell_rmNA), mc.cores=n.cores)
+                fsv.tmp <- mclapply(repid.tmp, getFireStats, mainDir=mainDir, years=years, cells=select(cells, -Cell_rmNA), mc.cores=n.cores)
                 fsv.dat[[j]] <- rbindlist(fsv.tmp)
                 rm(fsv.tmp)
                 gc()
@@ -149,7 +141,7 @@ if(doFire){
 	locs <- unique(fsv.dat$Location)
 	dir.create(fsvDir <- "/big_scratch/mfleonawicz/Rmpi/outputs/fsv", recursive=TRUE, showWarnings=FALSE)
 	for(j in 1:length(locs)){
-		filename.tmp <- paste0("fsv__", locs[j], "__", modnames[1]) # assume one model
+		filename.tmp <- paste0("fsv__", locs[j], "__", modname)
 		d.fsv <- fsv.dat[locs[j]]
 		save(d.fsv, locs, file=paste0(fsvDir, "/", filename.tmp, ".RData"))
 		print(paste(filename.tmp, "object", j, "of", length(locs), "saved."))
@@ -163,11 +155,11 @@ if(doFire){
 if(doAgeVeg){
     print("#### Compiling vegetation class and age statistics... ####")
 	if(Rmpi){
-        va.dat <- mpi.remote.exec( getAgeVegStats(i=repid[id], mainDir=mainDir, denDir=ageDenDir, years=years, cells=selct(cells, -Cell), n.samples=100) )
+        va.dat <- mpi.remote.exec( getAgeVegStats(i=repid[id], mainDir=mainDir, vegDir=vegDir, years=years, cells=select(cells, -Cell)) )
 	} else {
         len <- length(repid)
         if(len <= n.cores){
-            va.dat <- mclapply(repid, getAgeVegStats, mainDir=mainDir, denDir=ageDenDir, years=years, cells=selct(cells, -Cell), n.samples=100, mc.cores=n.cores)
+            va.dat <- mclapply(repid, getAgeVegStats, mainDir=mainDir, vegDir=vegDir, years=years, cells=select(cells, -Cell), mc.cores=n.cores)
         } else {
             serial.iters <- ceiling(len/n.cores)
             n.cores2 <- which(len/(1:n.cores) < serial.iters)[1]
@@ -175,42 +167,41 @@ if(doAgeVeg){
             for(j in 1:serial.iters){
                 repid.tmp <- 1:n.cores2 + (j-1)*n.cores2
                 repid.tmp <- repid.tmp[repid.tmp <= max(repid)]
-                va.dat.tmp <- mclapply(repid.tmp, getAgeVegStats, mainDir=mainDir, denDir=ageDenDir, years=years, cells=selct(cells, -Cell), n.samples=100, mc.cores=n.cores)
-                print(sapply(va.dat.tmp, class))
-                err.ind <- which(sapply(va.dat.tmp, class)=="try-error")
-                if(length(err.ind)) for(e in 1:length(err.ind)) print(va.dat.tmp[[e]])
-                print(va.dat.tmp[[1]])
-                va.dat[[j]] <- rbindlist(va.dat.tmp)
+                va.dat.tmp <- mclapply(repid.tmp, getAgeVegStats, mainDir=mainDir, vegDir=vegDir, years=years, cells=select(cells, -Cell), mc.cores=n.cores)
+                #print(sapply(va.dat.tmp, class))
+                #err.ind <- which(sapply(va.dat.tmp, class)=="try-error")
+                #if(length(err.ind)) for(e in 1:length(err.ind)) print(va.dat.tmp[[e]])
+                #print(va.dat.tmp[[1]])
+                #va.dat[[j]] <- rbindlist(va.dat.tmp)
                 rm(va.dat.tmp)
                 gc()
                 print(paste("Replicate batch", j, "of", serial.iters, "complete."))
             }
         }
     }
-    print("Veg area data frame list returned from slaves.")
 
-	va.dat <- rbindlist(va.dat)
-	va.dat[, Model := swapModelName(mod.scen[1])]
-	va.dat[, Scenario := swapScenarioName(mod.scen[2])]
-	va.dat[, Scenario := factor(Scenario, levels=scen.levels)]
-	va.dat[, Phase := getPhase(mod.scen[2])]
-	va.dat <- setcolorder(va.dat, c("Phase", "Scenario", "Model", "LocGroup", "Location", "VegID", "Var", "Val", "Year", "Replicate"))
-	setkey(va.dat, Location)
-	print("Converted list to single veg area data table.")
-	print("Saving veg area data tables by location to .RData files.")
+	#va.dat <- rbindlist(va.dat)
+	#va.dat[, Model := swapModelName(mod.scen[1])]
+	#va.dat[, Scenario := swapScenarioName(mod.scen[2])]
+	#va.dat[, Scenario := factor(Scenario, levels=scen.levels)]
+	#va.dat[, Phase := getPhase(mod.scen[2])]
+	#va.dat <- setcolorder(va.dat, c("Phase", "Scenario", "Model", "LocGroup", "Location", "Vegetation", "Age", "Area", "Year", "Replicate"))
+	#setkey(va.dat, Location)
+	print("Vegetation area by age completed.")
+	#print("Saving veg area by age data tables by location to .RData files.")
 
-	locs <- unique(va.dat$Location)
-	dir.create(vegDir <- "/big_scratch/mfleonawicz/Rmpi/outputs/veg", showWarnings=F)
+	#locs <- unique(va.dat$Location)
+	#dir.create(vegAreaDir <- "/big_scratch/mfleonawicz/Rmpi/outputs/vegArea", showWarnings=F)
 
-	for(j in 1:length(locs)){
-		filename.tmp <- paste0("veg__", locs[j], "__", modnames[1]) # assume one model
-		d.veg <- va.dat[locs[j]]
-		save(d.veg, locs, file=paste0(vegDir, "/", filename.tmp, ".RData"))
-		print(paste(filename.tmp, "object", j, "of", length(locs), "saved."))
-	}
-	print(tables())
-	rm(va.dat, d.veg)
-	gc()
+	#for(j in 1:length(locs)){
+	#	filename.tmp <- paste0("veg__", locs[j], "__", modname)
+	#	d.veg <- va.dat[locs[j]]
+	#	save(d.veg, locs, file=paste0(vegDir, "/", filename.tmp, ".RData"))
+	#	print(paste(filename.tmp, "object", j, "of", length(locs), "saved."))
+	#}
+	#print(tables())
+	#rm(va.dat, d.veg)
+	#gc()
 }
 
 # All done
