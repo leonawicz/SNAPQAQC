b27508a7162d85cdc2bc7934d96cc4a429f9384b
leonawicz <matt.rstat@gmail.com>
2017-01-30 19:17:52 -0700

    Update bash script

--- code/alfresco/copy_alf_outputs_AK.sh
+++ code/alfresco/copy_alf_outputs_AK.sh
@@ -5,10 +5,14 @@ model=$2 # e.g., cccma_cgcm3_1.sresa1b, model directory
 period=$3 # historical or not (i.e., projected), indicate if years specified below are historical CRU 3.2 data
 gbm=$4 # 3m or 5m
 
+copy=${gbm}
+len=${#copy}
+for((i=$len-1;i>=0;i--)); do gbm2="$gbm2${copy:$i:1}"; done
+
 if [ "$period" == "historical" ]
 then
  modelOut=CRU32 # renaming destination model directory 
- yr1=1900
+ yr1=1950
  yr2=2013
 else
  modelOut=$model # leaving destination directory same as source
@@ -16,7 +20,7 @@ else
  yr2=2099
 fi
 
-inDir=/big_scratch/shiny/Runs_Statewide/paul.duffy_at_neptuneinc.org/*$rcp*$model/Maps # source Maps directory
+inDir=/big_scratch/shiny/Runs_Statewide/paul.duffy_at_neptuneinc.org/$gbm2*$rcp*$model/Maps # source Maps directory
 outDir=/atlas_scratch/mfleonawicz/alfresco/CMIP5_Statewide/outputs/$gbm/$rcp.$modelOut/Maps # destination Maps directory
 mkdir -p $outDir
 
